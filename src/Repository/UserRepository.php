22fd8e8d14de43280f51c02ae5f34ef372eaf914
Fabien Brossier <fabien.brossier@bcubix.com>
2022-08-03 14:36:20 +0200

    Minor correction.

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,11 +5,25 @@ namespace App\Repository;
 use App\Entity\User;
 
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
 
-class UserRepository extends CrudRepository
+class UserRepository extends CrudRepository implements UserLoaderInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
     }
+
+    public function loadUserByIdentifier(string $email): ?User
+    {
+        $email = strtolower($email);
+
+        return $this->createQueryBuilder('o')
+            ->where('u.email LIKE :email')
+            ->andWhere('u.active = 1')
+            ->setParameter('email', $email)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+   }
 }
