b77f3d7b8dbb01aa51dbfac781be1ec18ed681c8
Jeanne Cai <jeanne.cai@bcubix.com>
2022-09-26 12:57:24 +0200

    Added main dashboard without children

---
+++ assets/Apps/Home/MainDashboard.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { CmtPageWrapper } from '@Components/CmtPage/CmtPageWrapper/CmtPageWrapper';
+import { Grid } from '@mui/material';
+import { dashboardSelector, getDashboardAction } from '@Redux/dashboard/dashboardSlice';
+
+const MainDashboard = () => {
+    const { loading, dashboard, error } = useSelector(dashboardSelector);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (!loading && !dashboard && !error) {
+            dispatch(getDashboardAction());
+        }
+    }, []);
+
+    return (
+        <CmtPageWrapper title={'Tableau de bord'}>
+            { dashboard && (
+                <Grid container spacing={2} sx={{ marginTop: 0 }}>
+                    <Grid item xs={12} md={4} lg={3}></Grid>
+                    <Grid item xs={12} md={8} lg={7}></Grid>
+                    <Grid item xs={12} md={2} lg={2}></Grid>
+                </Grid>
+            )}
+        </CmtPageWrapper>
+    );
+};
+
+export default MainDashboard;
