99562137e79088113f7e4dc623608fa05c8361c5
Jeanne Cai <jeanne.cai@bcubix.com>
2022-11-24 10:15:25 +0100

    Fixed typo

--- src/Controller/Website/HomeController.php
+++ src/Controller/Website/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Website;
 use App\Entity\Parameter\Parameter;
 use App\Entity\Theme\Theme;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends WebsiteController
@@ -15,14 +14,14 @@ class HomeController extends WebsiteController
     {
         $result = $this->em->getRepository(Parameter::class)->findAllForAdmin(['paramKey' => 'main_theme']);
         if (null === $result || count($result['results']) !== 1) {
-            throw new \Exception(Response::HTTP_NOT_FOUND, 1404, "Le thème principal n'est pas dans les paramètres.");
+            throw new \Exception("Le thème principal n'est pas dans les paramètres.");
         }
 
         $parameter = $result['results'][0];
 
         $theme = $this->em->getRepository(Theme::class)->findOneForAdmin($parameter->getParamValue());
         if (null === $theme) {
-            throw new \Exception(Response::HTTP_NOT_FOUND, 1404, "Le id du thème n'appartient à aucun thème existant.");
+            throw new \Exception('Il n\'y a pas de thème principal.');
         }
 
         return $this->render('@website/index.html.twig', [
