36669c8432ebf48a6de70058e975c155337af5b6
Bryan Bouillot <bryanbouillot@gmail.com>
2022-11-28 10:33:28 +0100

    [ADD]: Added tests E2E for updating page

---
+++ assets/Admin/tests/cypress/e2e/Pages/updatePage.cy.js
@@ -0,0 +1,31 @@
+import { Constant } from '../../../../AdminService/Constant';
+import {
+    ADMIN_API_BASE_PATH,
+    PAGES_API_PATH,
+    USER_EMAIL,
+    USER_PASSWORD,
+} from '../../../cypress.constant';
+
+describe('Create Page Spec', () => {
+    beforeEach(() => {
+        cy.login(USER_EMAIL, USER_PASSWORD);
+
+        cy.intercept('POST', ADMIN_API_BASE_PATH + PAGES_API_PATH + '/*').as('updatePage');
+        cy.visit(Constant.PAGES_BASE_PATH + '/1' + Constant.EDIT_PATH);
+        cy.wait(500);
+    });
+
+    it('Update Page', () => {
+        cy.get('#title').clear().type('Test Page 1 MODIFY');
+        cy.get('#pageBlocks-0-contentControl .ck-content').clear().type('Test Block 1 MODIFY');
+        cy.get('#active').click();
+
+        cy.get('#submitForm').click();
+
+        cy.wait('@updatePage').then(({ response }) => {
+            expect(response.statusCode).to.eq(200);
+            expect(response.body.title).to.eq('Test Page 1 MODIFY');
+            expect(response.body.pageBlocks.at(0).content).contains('Test Block 1 MODIFY');
+        });
+    });
+});
