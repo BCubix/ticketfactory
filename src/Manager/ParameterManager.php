70a6250734112e802439288a8cc888042c7a7d15
Jeanne Cai <jeanne.cai@bcubix.com>
2022-11-28 12:48:55 +0100

    Fixed typo

--- src/Manager/ParameterManager.php
+++ src/Manager/ParameterManager.php
@@ -11,12 +11,12 @@ class ParameterManager extends AbstractManager
     public function get(string $key): Parameter
     {
         $result = $this->em->getRepository(Parameter::class)->findAllForAdmin(['paramKey' => $key]);
-        if (1 !== $result['count']) {
+        if (1 !== $result['total']) {
             throw new ApiException(Response::HTTP_NOT_FOUND, 1404,
                 "Le paramètre avec la clé $key n'existe pas ou existe en plusieurs.");
         }
 
-        return $result['result'][0];
+        return $result['results'][0];
     }
 
     public function set(string $key, $newValue)
