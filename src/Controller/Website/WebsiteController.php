409a91842c4c6b2571ad92fede4aa8e7460387fb
Jeanne Cai <jeanne.cai@bcubix.com>
2022-12-08 14:52:09 +0100

    Added hook service in website

--- src/Controller/Website/WebsiteController.php
+++ src/Controller/Website/WebsiteController.php
@@ -2,15 +2,19 @@
 
 namespace App\Controller\Website;
 
+use App\Service\Hook\HookService;
+
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 
 abstract class WebsiteController extends AbstractFOSRestController
 {
     protected $em;
+    protected $hs;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, HookService $hs)
     {
         $this->em = $em;
+        $this->hs = $hs;
     }
 }
