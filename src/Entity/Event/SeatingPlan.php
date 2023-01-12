4e05f4d3f1b52954826eb5a1513f16d633372108
Jeanne Cai <jeanne.cai@bcubix.com>
2023-01-12 10:20:29 +0100

    Added repository for seating plan

--- src/Entity/Event/SeatingPlan.php
+++ src/Entity/Event/SeatingPlan.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity\Event;
 
+use App\Repository\SeatingPlanRepository;
+
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as JMS;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[JMS\ExclusionPolicy('all')]
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: SeatingPlanRepository::class)]
 class SeatingPlan
 {
     /*** > Trait ***/
