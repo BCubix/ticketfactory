7e627059cc525841c462611e914ab274743ddeca
Bryan Bouillot <bryanbouillot@gmail.com>
2023-05-10 17:20:25 +0200

    [FIX]: Fixed classical pages

--- src/Controller/Website/PageController.php
+++ src/Controller/Website/PageController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends WebsiteController
 {
-    public function index(string $slugs)
+    public function index(Page $page)
     {
         $userAddress = $this->getRequest()->get('u');
         $userPass = $this->getRequest()->get('t');
@@ -21,33 +21,6 @@ class PageController extends WebsiteController
             $user = $this->em->getRepository(User::class)->getUserByTokenForWebsite($userAddress, $userPass);
         }
 
-        $slugs = explode('/', $slugs);
-        array_filter($slugs, function($value) {
-            return !empty($value);
-        });
-
-        if (count($slugs) > 1 && empty($slugs[count($slugs) - 1])) {
-            array_pop($slugs);
-            $slugs = implode('/', $slugs);
-            $params = array_merge(['slugs' => $slugs], $this->getRequest()->query->all());
-
-            return $this->redirectToRoute('tf_website_orchestrator', $params);
-        }
-
-        $refPage = null;
-        foreach ($slugs as $slug) {
-            $page = $this->mf->get('page')->getBySlug($slug);
-            if (null === $page) {
-                break;
-            }
-
-            $this->em->detach($page);
-
-            if ((null === $refPage) || ($refPage->getId() == $page->getParent()->getId())) {
-                $refPage = $page;
-                array_shift($slugs);
-            }
-        }
 
         if (null === $page || (false === $page->isActive() && (null === $user || !in_array("ROLE_ADMIN", $user->getRoles())))) {
             throw $this->createNotFoundException('This page does not exist.');
