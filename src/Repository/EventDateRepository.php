dc0c802f3e13238a74e70492470f3d1b5474635c
Bryan Bouillot <bryanbouillot@gmail.com>
2023-09-05 12:15:49 +0200

    [REMOVE]: Removed getCalendar date event from base src

--- src/Repository/EventDateRepository.php
+++ src/Repository/EventDateRepository.php
@@ -46,57 +46,6 @@ class EventDateRepository extends CrudRepository
         ;
     }
 
-    public function getCalendarForWebsite(\DateTime $beginDate, \DateTime $endDate, array $filters)
-    {
-        $categoryId = (empty($filters['eventCategory']) ? null : $filters['eventCategory']->getId());
-        $roomId     = (empty($filters['room']) ? null : $filters['room']->getId());
-        $firstDay   = (empty($filters['firstDay']) ? null : $filters['firstDay']);
-
-        $results = $this
-            ->createQueryBuilder('ed')
-            ->addSelect('edb')
-            ->addSelect('e')
-            ->addSelect('ec')
-            ->addSelect('r')
-            ->innerjoin('ed.eventDateBlock', 'edb')
-            ->innerjoin('edb.event', 'e')
-            ->innerjoin('e.eventCategories', 'ec')
-            ->leftJoin('e.room', 'r')
-            ->where('e.active = 1')
-            ->andWhere('ec.active = 1')
-            ->andWhere('(ed.eventDate BETWEEN :beginDate AND :endDate) OR (ed.reportDate BETWEEN :beginDate AND :endDate)')
-        ;
-
-        if (null !== $categoryId) {
-            $results = $results
-                ->andWhere('ec.id = :categoryId')
-                ->setParameter('categoryId', $categoryId)
-            ;
-        }
-
-        if (null !== $roomId) {
-            $results = $results
-                ->andWhere('r.id = :roomId')
-                ->setParameter('roomId', $roomId)
-            ;
-        }
-
-        if (null !== $firstDay) {
-            $results = $results
-                ->andWhere('ed.eventDate > :firstDay')
-                ->setParameter('firstDay', new \DateTime($firstDay))
-            ;
-        }
-
-        return $results
-            ->setParameter('beginDate', $beginDate)
-            ->setParameter('endDate', $endDate)
-            ->orderBy('ed.eventDate', 'ASC')
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-
     public function findOneByIdForWebsite(int $id, ?int $eventId = null): ?EventDate {
         $result = $this->createQueryBuilder("ed")
             ->addSelect("edb")
