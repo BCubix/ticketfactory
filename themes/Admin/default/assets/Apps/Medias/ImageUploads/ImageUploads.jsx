6592fd0892b3c5297b19e78accff20f5b4cb7cc1
Lucas Moritel <lucas.moritel@epitech.eu>
2023-11-07 14:53:26 +0100

    [ADD] flex on the list of image when upload images

--- themes/Admin/default/assets/Apps/Medias/ImageUploads/ImageUploads.jsx
+++ themes/Admin/default/assets/Apps/Medias/ImageUploads/ImageUploads.jsx
@@ -5,7 +5,13 @@ export const ImageUploads = ({ imageUploads, onSelect }) => {
         <Box sx={{ width: 100, borderRight: '1px solid gray', position: 'absolute', right: '100%' }}>
             <Box component="ul" sx={{ listStyleType: 'none', padding: 0 }}>
                 {imageUploads.map((image) => (
-                    <Box component="li" sx={{ marginBottom: 3 }} key={image.id} onClick={() => onSelect(image.id)} style={{ margin: 1 }}>
+                    <Box
+                        component="li"
+                        sx={{ marginBottom: 3 }}
+                        key={image.id}
+                        onClick={() => onSelect(image.id)}
+                        style={{ margin: 1, display: 'flex', flexDirection: 'column', alignItems: 'flex-end' }}
+                    >
                         <Box
                             component="img"
                             src={image.documentUrl}
