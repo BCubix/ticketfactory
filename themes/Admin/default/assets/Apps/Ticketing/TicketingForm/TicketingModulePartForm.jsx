9d8000a6853ac467de822cf7d8d1b97f076d7806
Bryan BOUILLOT <bryanbouillot@gmail.com>
2024-03-18 10:13:17 +0100

    Added tooltip on disabled options for ticketing modules

--- themes/Admin/default/assets/Apps/Ticketing/TicketingForm/TicketingModulePartForm.jsx
+++ themes/Admin/default/assets/Apps/Ticketing/TicketingForm/TicketingModulePartForm.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo } from 'react';
 import { NotificationManager } from 'react-notifications';
-import { FormControlLabel, FormLabel, Grid, Radio, RadioGroup, Switch } from '@mui/material';
+import { FormControlLabel, FormLabel, Grid, Radio, RadioGroup, Switch, Tooltip } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { Component } from '@/AdminService/Component';
 
@@ -62,9 +62,17 @@ export const TicketingModulePartForm = ({ setFieldValue, handleChange, values, f
                 <Grid item xs={12} sm={2} sx={{ borderRight: '1px solid #d3d3d3' }}>
                     <FormLabel id="ticketing-type-label">Type d'utilisation</FormLabel>
                     <RadioGroup aria-labelledby="ticketing-type-label" defaultValue="" name="type" value={values.type || ''} onChange={handleChange}>
-                        <FormControlLabel value="api" control={<Radio />} label="Api" disabled={!Boolean(availableTypes?.api)} />
-                        <FormControlLabel value="iframe" control={<Radio />} label="Iframe" disabled={!Boolean(availableTypes?.iframe)} />
-                        <FormControlLabel value="external" control={<Radio />} label="Externe" disabled={!Boolean(availableTypes?.external)} />
+                        <Tooltip title={!Boolean(availableTypes?.api) ? 'Type non pris en charge par le module.' : ''}>
+                            <FormControlLabel value="api" control={<Radio />} label="Api" disabled={!Boolean(availableTypes?.api)} />
+                        </Tooltip>
+
+                        <Tooltip title={!Boolean(availableTypes?.iframe) ? 'Type non pris en charge par le module.' : ''}>
+                            <FormControlLabel value="iframe" control={<Radio />} label="Iframe" disabled={!Boolean(availableTypes?.iframe)} />
+                        </Tooltip>
+
+                        <Tooltip title={!Boolean(availableTypes?.external) ? 'Type non pris en charge par le module.' : ''}>
+                            <FormControlLabel value="external" control={<Radio />} label="Externe" disabled={!Boolean(availableTypes?.external)} />
+                        </Tooltip>
                     </RadioGroup>
                 </Grid>
 
@@ -79,34 +87,47 @@ export const TicketingModulePartForm = ({ setFieldValue, handleChange, values, f
 
                         {values?.type === 'api' && (
                             <Grid item xs={12}>
-                                <FormControlLabel
-                                    control={
-                                        <Switch
-                                            checked={Boolean(values?.catalogSynchronization)}
-                                            name="catalogSynchronization"
-                                            onChange={handleChange}
-                                            disabled={!Boolean(apiOptions?.catalogSynchronization)}
-                                        />
-                                    }
-                                    label="Synchronisation catalogue"
-                                />
-                                <FormControlLabel
-                                    control={
-                                        <Switch
-                                            checked={Boolean(values?.customerProfile)}
-                                            name="customerProfile"
-                                            onChange={handleChange}
-                                            disabled={!Boolean(apiOptions?.customerProfile)}
-                                        />
-                                    }
-                                    label="Profil client"
-                                />
-                                <FormControlLabel
-                                    control={
-                                        <Switch checked={Boolean(values?.orderTunnel)} name="orderTunnel" onChange={handleChange} disabled={!Boolean(apiOptions?.orderTunnel)} />
-                                    }
-                                    label="Tunnel de commande"
-                                />
+                                <Tooltip title={!Boolean(apiOptions?.catalogSynchronization) ? 'Fonctionnalité non prise en charge par le module.' : ''}>
+                                    <FormControlLabel
+                                        control={
+                                            <Switch
+                                                checked={Boolean(values?.catalogSynchronization)}
+                                                name="catalogSynchronization"
+                                                onChange={handleChange}
+                                                disabled={!Boolean(apiOptions?.catalogSynchronization)}
+                                            />
+                                        }
+                                        label="Synchronisation catalogue"
+                                    />
+                                </Tooltip>
+
+                                <Tooltip title={!Boolean(apiOptions?.customerProfile) ? 'Fonctionnalité non prise en charge par le module.' : ''}>
+                                    <FormControlLabel
+                                        control={
+                                            <Switch
+                                                checked={Boolean(values?.customerProfile)}
+                                                name="customerProfile"
+                                                onChange={handleChange}
+                                                disabled={!Boolean(apiOptions?.customerProfile)}
+                                            />
+                                        }
+                                        label="Profil client"
+                                    />
+                                </Tooltip>
+
+                                <Tooltip title={!Boolean(apiOptions?.orderTunnel) ? 'Fonctionnalité non prise en charge par le module.' : ''}>
+                                    <FormControlLabel
+                                        control={
+                                            <Switch
+                                                checked={Boolean(values?.orderTunnel)}
+                                                name="orderTunnel"
+                                                onChange={handleChange}
+                                                disabled={!Boolean(apiOptions?.orderTunnel)}
+                                            />
+                                        }
+                                        label="Tunnel de commande"
+                                    />
+                                </Tooltip>
                             </Grid>
                         )}
                     </Grid>
