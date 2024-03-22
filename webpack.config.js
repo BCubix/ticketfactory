fb32c9a618fc489cce13bb36f93c7d9bbb54b137
Bryan BOUILLOT <bryanbouillot@gmail.com>
2024-03-22 10:14:48 +0100

    [CHANGE]: Minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 // <<< Variables
 const adminThemeName = 'default';
-const websiteThemeName = 'default';
+const websiteThemeName = 'Chouette';
 // >>> Variables
 
 var Encore = require('@symfony/webpack-encore');
