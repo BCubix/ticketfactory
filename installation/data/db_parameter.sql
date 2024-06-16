2f7d21e60672ef4f615ee590ffbbe0310bc46c74
Bryan BOUILLOT <bryan.bouillot@bcubix.com>
2024-06-16 11:36:15 +0200

    [FIX]: Minor fix

--- installation/data/db_parameter.sql
+++ installation/data/db_parameter.sql
@@ -3,22 +3,22 @@ INSERT INTO
         id, name, type, param_key, param_value, available_value, validations, tab_name, block_name, breakpoints_value, general_parameter
     )
 VALUES
-    (1, 'Type d\'évènements par défaut', 'list', 'core_default_events_type', 'Pièces', '[{\"id\": \"Pièces\", \"name\": \"Pièces de théatre\"}, {\"id\": \"Expositions\", \"name\": \"Expositions temporaires\"}, {\"id\": \"Films\", \"name\": \"Diffusions de films\"}, {\"id\": \"Concerts\", \"name\": \"Concerts\"}, {\"id\": \"Ballets\", \"name\": \"Ballets\"}]', '[]', 'Général', 'Types d\'événements', 'xs-12 md-6', 1);
-    (2, 'Type de structures par défaut', 'list', 'core_default_structures_type', '0', '[{\"id\": 0, \"name\": \"Théatre\"}, {\"id\": 1, \"name\": \"Musée\"}, {\"id\": 2, \"name\": \"Cinéma\"}, {\"id\": 3, \"name\": \"Festival\"}]', '[]', 'Général', 'Types d\'événements', 'xs-12 md-6', 1);
+    (1, "Type d'évènements par défaut", 'list', 'core_default_events_type', 'Pièces', '[{\"id\": \"Pièces\", \"name\": \"Pièces de théatre\"}, {\"id\": \"Expositions\", \"name\": \"Expositions temporaires\"}, {\"id\": \"Films\", \"name\": \"Diffusions de films\"}, {\"id\": \"Concerts\", \"name\": \"Concerts\"}, {\"id\": \"Ballets\", \"name\": \"Ballets\"}]', '[]', 'Général', "Types d'événements", 'xs-12 md-6', 1);
+    (2, 'Type de structures par défaut', 'list', 'core_default_structures_type', '0', '[{\"id\": 0, \"name\": \"Théatre\"}, {\"id\": 1, \"name\": \"Musée\"}, {\"id\": 2, \"name\": \"Cinéma\"}, {\"id\": 3, \"name\": \"Festival\"}]', '[]', 'Général', "Types d'événements", 'xs-12 md-6', 1);
     (3, 'Format des images', 'string', 'core_image_format', '18', NULL, '[]', NULL, NULL, NULL, 1);
     (4, 'Cropper ou compléter les images ?', 'bool', 'core_image_to_crop', '1', NULL, '[]', NULL, NULL, NULL, 1);
-    (5, 'Qualité d\'image WEBP', 'int', 'core_image_webp_quality', '90', NULL, '[]', NULL, NULL, NULL, 1);
-    (6, 'Qualité d\'image PNG', 'int', 'core_image_png_quality', '90', NULL, '[]', NULL, NULL, NULL, 1);
-    (7, 'Qualité d\'image JPG', 'int', 'core_image_jpg_quality', '90', NULL, '[]', NULL, NULL, NULL, 1);
+    (5, "Qualité d'image WEBP", 'int', 'core_image_webp_quality', '90', NULL, '[]', NULL, NULL, NULL, 1);
+    (6, "Qualité d'image PNG", 'int', 'core_image_png_quality', '90', NULL, '[]', NULL, NULL, NULL, 1);
+    (7, "Qualité d'image JPG", 'int', 'core_image_jpg_quality', '90', NULL, '[]', NULL, NULL, NULL, 1);
     (8, 'Thème principal (admin)', 'string', 'core_admin_theme', 'default', NULL, '[]', NULL, NULL, NULL, 1);
     (9, 'Thème principal', 'string', 'core_main_theme', 'default', NULL, '[]', NULL, NULL, NULL, 1);
     (10, '1er mois de la saison', 'list', 'core_season_month', '9', '[{\"id\": 1, \"name\": \"Janvier\"}, {\"id\": 2, \"name\": \"Février\"}, {\"id\": 3, \"name\": \"Mars\"}, {\"id\": 4, \"name\": \"Avril\"}, {\"id\": 5, \"name\": \"Mai\"}, {\"id\": 6, \"name\": \"Juin\"}, {\"id\": 7, \"name\": \"Juillet\"}, {\"id\": 8, \"name\": \"Août\"}, {\"id\": 9, \"name\": \"Septembre\"}, {\"id\": 10, \"name\": \"Octobre\"}, {\"id\": 11, \"name\": \"Novembre\"}, {\"id\": 12, \"name\": \"Décembre\"}]', NULL, 'Evènements', 'Saison', 'xs-12 md-6', 1);
     (11, 'Nom', 'string', 'core_website_name', '', NULL, NULL, NULL, NULL, NULL, 1);
-    (12, 'Adresse email d\'envoi', 'string', 'core_email_sender', NULL, NULL, NULL, 'Général', 'Email', 'xs-12 md-6', 1);
-    (13, 'Formats d\'images par défaut', 'ImageFormat', 'core_default_image_formats', NULL, NULL, '[]', 'Médias', 'Formats d\'images', 'xs-12 md-6', 1);
+    (12, "Adresse email d'envoi", 'string', 'core_email_sender', NULL, NULL, NULL, 'Général', 'Email', 'xs-12 md-6', 1);
+    (13, "Formats d'images par défaut", 'ImageFormat', 'core_default_image_formats', NULL, NULL, '[]', 'Médias', "Formats d'images", 'xs-12 md-6', 1);
     (14, 'Utiliser les produits', 'bool', 'core_use_products', '1', '[]', '[]', 'Général', 'Fonctionnalités', 'xs-12 md-6', 1);
     (15, 'Utiliser les clients', 'bool', 'core_use_customers', '1', '[]', '[]', 'Général', 'Fonctionnalités', 'xs-12 md-6', 1);
-    (16, 'Utiliser le tunnel d\'achat', 'bool', 'core_use_purchase', '1', '[]', '[]', 'Général', 'Fonctionnalités', 'xs-12 md-6', 1);
+    (16, "Utiliser le tunnel d'achat", 'bool', 'core_use_purchase', '1', '[]', '[]', 'Général', 'Fonctionnalités', 'xs-12 md-6', 1);
     (17, 'Filtrer par saison', 'bool', 'core_event_season_filter', '1', NULL, '[]', 'Evènements', 'Filtres', 'xs-12 md-6 lg-4', 1);
     (18, 'Filtrer par catégorie', 'bool', 'core_event_category_filter', '1', NULL, '[]', 'Evènements', 'Filtres', 'xs-12 md-6 lg-4', 1);
     (19, 'Filtrer par salle', 'bool', 'core_event_room_filter', '1', NULL, '[]', 'Evènements', 'Filtres', 'xs-12 md-6 lg-4', 1);
@@ -28,9 +28,9 @@ VALUES
     (23, 'Activer le mode maintenance', 'bool', 'core_maintenance_mode', '0', NULL, '[]', 'Général', 'Maintenance du site', 'xs-12', 1);
     (24, 'Message de maintenance', 'string', 'core_maintenance_message', 'Nous sommes actuellement fermé, revenez plus tard.', NULL, '[]', 'Général', 'Maintenance du site', 'xs-12 md-6', 1);
     (25, 'Adresses IP autorisées (séparées par des virgules (0.0.0.0, 192.168.0.1, ...))', 'string', 'core_maintenance_ip', NULL, NULL, '[]', 'Général', 'Maintenance du site', 'xs-12 md-6', 1);
-    (26, 'Nom par défaut des groupes de dates', 'string', 'core_default_event_date_block_name', NULL, NULL, '[]', 'Evènements', 'Dates d\'évènements', 'xs-12 md-6 lg-4', 1);
-    (27, 'Nom par défaut des groupes de tarifs', 'string', 'core_default_event_price_block_name', NULL, NULL, '[]', 'Evènements', 'Tarifs d\'évènements', 'xs-12 md-6 lg-4', 1);
-    (28, 'Tarifs par défaut', 'prices', 'core_default_event_price', '[]', NULL, '[]', 'Evènements', 'Tarifs d\'évènements', 'xs-12', 1);
+    (26, 'Nom par défaut des groupes de dates', 'string', 'core_default_event_date_block_name', NULL, NULL, '[]', 'Evènements', "Dates d'évènements", 'xs-12 md-6 lg-4', 1);
+    (27, 'Nom par défaut des groupes de tarifs', 'string', 'core_default_event_price_block_name', NULL, NULL, '[]', 'Evènements', "Tarifs d'évènements", 'xs-12 md-6 lg-4', 1);
+    (28, 'Tarifs par défaut', 'prices', 'core_default_event_price', '[]', NULL, '[]', 'Evènements', "Tarifs d'évènements", 'xs-12', 1);
     (29, 'Hôte du serveur', 'string', 'core_email_host', 'smtp.gmail.com', NULL, NULL, 'Général', 'Email', 'xs-12 md-8', 1);
     (30, 'Port du serveur', 'string', 'core_email_port', '465', NULL, NULL, 'Général', 'Email', 'xs-12 md-4', 1);
     (31, 'Identifiant du serveur', 'string', 'core_email_user', NULL, NULL, NULL, 'Général', 'Email', 'xs-12 md-6', 1);
@@ -40,14 +40,14 @@ VALUES
     (35, 'Afficher les saisons', 'bool', 'core_display_seasons', '1', '[]', '[]', 'Général', 'Paramètres du site', 'xs-12 md-4', 1);
     (36, 'Afficher les tags', 'bool', 'core_display_tags', '1', '[]', '[]', 'Général', 'Paramètres du site', 'xs-12 md-4', 1);
     (37, 'Poids maximum des fichiers joints (Mo)', 'int', 'core_max_files_size', '10', '[]', '[]', 'Général', 'Fichiers', 'xs-12 md-6', 1);
-    (38, 'Taille maximale des images d\'événements (Mo)', 'int', 'core_max_images_size', '10', '[]', '[]', 'Général', 'Fichiers', 'xs-12 md-6', 1);
+    (38, "Taille maximale des images d'événements (Mo)", 'int', 'core_max_images_size', '10', '[]', '[]', 'Général', 'Fichiers', 'xs-12 md-6', 1);
     (39, 'Générer les fichiers robot.txt et sitemap.xml', 'requestButton', 'core_generate_seo', '/parametres/generer/seo', NULL, NULL, 'Paramètres généraux', 'Seo', 'xs-12 md-6', 1);
     (40, 'Activer le mode catalogue', 'bool', 'core_catalog_mode', '0', '[]', '[]', 'Général', 'Paramètres du site', 'xs-12 md-4', 1);
     (41, 'Différer la désactivation des spectacles (Heure)', 'int', 'core_event_additional_time', '3', '[]', '[]', 'Général', 'Paramètres du site', 'xs-12 md-4', 1);
     (42, 'Utiliser les saisons', 'bool', 'core_use_seasons', '1', NULL, NULL, 'Evènements', 'Saison', 'xs-12 md-6', 1);
     (43, 'Utiliser les salles', 'bool', 'core_use_rooms', '1', NULL, NULL, 'Evènements', 'Salles', 'xs-12 md-6', 1);
-    (44, 'Utiliser les types d\'évènements', 'bool', 'core_use_event_types', '1', NULL, NULL, 'Evènements', 'Types', 'xs-12 md-6', 1);
-    (45, 'Nombre d\'évènements à afficher par page', 'int', 'core_event_limit', '12', NULL, '[]', 'Evènements', 'Pages listes', 'xs-12 md-6', 1);
+    (44, "Utiliser les types d'évènements", 'bool', 'core_use_event_types', '1', NULL, NULL, 'Evènements', 'Types', 'xs-12 md-6', 1);
+    (45, "Nombre d'évènements à afficher par page", 'int', 'core_event_limit', '12', NULL, '[]', 'Evènements', 'Pages listes', 'xs-12 md-6', 1);
     (46, 'Tri par défaut', 'list', 'core_event_default_sort', 'chronoAsc', '[{\"id\": \"nameAsc\", \"name\": \"Nom (Ordre alphabétique)\"}, {\"id\": \"nameDesc\", \"name\": \"Nom (Ordre anti-alphabétique)\"}, {\"id\": \"chronoAsc\", \"name\": \"Date (Ordre chronologique)\"}, {\"id\": \"chronoDesc\", \"name\": \"Date (Ordre antéchronologique)\"}]', NULL, 'Evènements', 'Pages listes', 'xs-12 md-6', 1);
     (47, 'Filtrer par tags', 'bool', 'core_event_tag_filter', '1', NULL, '[]', 'Evènements', 'Filtres', 'xs-12 md-6 lg-4', 1);
     (48, 'Filtrer par type', 'bool', 'core_event_type_filter', '1', NULL, '[]', 'Evènements', 'Filtres', 'xs-12 md-6 lg-4', 1);
